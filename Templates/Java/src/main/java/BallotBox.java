ffd3e8718a9385fd375607cdfe78ea1e2366e6aa
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-21 21:34:47 -0400

    Added method to take CSV files as input

--- Templates/Java/src/main/java/BallotBox.java
+++ Templates/Java/src/main/java/BallotBox.java
@@ -1,2 +1,24 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class BallotBox {
+    private String[] names;
+    private List<Ballot> ballots= new ArrayList<>();
+
+    public BallotBox (String file) {
+        try (Scanner sc = new Scanner(new File(file))) {
+            names = sc.nextLine().split(",");
+            while (sc.hasNextLine()) {
+                int[] votes = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+                ballots.add(new Ballot(
+                        Arrays.copyOfRange(votes,1, votes.length)));
+                System.out.println(ballots.getLast());
+            }
+        } catch (Exception e) {
+            System.out.println("No File Found");
+        }
+    }
 }

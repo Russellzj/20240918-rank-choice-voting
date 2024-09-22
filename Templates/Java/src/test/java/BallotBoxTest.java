30c18b4689ee0d3bf0662466f514e39dceb45ed0
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-21 22:10:03 -0400

    Added test for no file and csv file

--- Templates/Java/src/test/java/BallotBoxTest.java
+++ Templates/Java/src/test/java/BallotBoxTest.java
@@ -1,17 +1,23 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BallotBoxTest {
     BallotBox ballotBox;
-
-    @BeforeEach
-    public void beforeEach() {
-        ballotBox = new BallotBox("");
-    }
+    String csvTest1 = "../../test-elections/test-01-basic-majority/cvr.csv";
 
 
     @Test
-    public void testBallotBox() { }
+    @DisplayName("No File Test")
+    public void testBallotBox() {
+        BallotBox noFile = new BallotBox("");
+    }
+    @Test
+    @DisplayName("Fill Ballets With CSV")
+    public void testFillBallotBox() {
+        ballotBox = new BallotBox(csvTest1);
 
+    }
 
 }

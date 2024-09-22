2b4bfd19b65f6b1af778bb292e6315e16f24b781
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-21 21:19:20 -0400

    Variables and get created

--- Templates/Java/src/main/java/Ballot.java
+++ Templates/Java/src/main/java/Ballot.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+
 public class Ballot {
+    int voteLocation = 0;
+    int[] votes;
+
+    public Ballot(int[] votes) {
+        this.votes = votes;
+    }
+
+    public int getChoice() {
+        return votes[voteLocation];
+    }
+
 }

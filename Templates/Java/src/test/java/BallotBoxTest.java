f33f3be30b17cd5fea1b6a481ca0e370db1c38cb
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-21 21:39:03 -0400

    Created eempty test for BallotBox

--- Templates/Java/src/test/java/BallotBoxTest.java
+++ Templates/Java/src/test/java/BallotBoxTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BallotBoxTest {
+    BallotBox ballotBox;
+
+    @BeforeEach
+    public void beforeEach() {
+        ballotBox = new BallotBox("");
+    }
+
+
+    @Test
+    public void testBallotBox() { }
+
+
 }

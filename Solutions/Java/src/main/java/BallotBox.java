d2ef29f8499f05aadd5bcc2f0493b101fab5b5ce
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-23 11:59:36 -0400

    Candidates class add and method to assign votes and get winner

--- Solutions/Java/src/main/java/BallotBox.java
+++ Solutions/Java/src/main/java/BallotBox.java
@@ -5,12 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BallotBox {
-    private String[] names;
+    //Holds candidate's names and total votes
+    private List<Candidate> candidates = new ArrayList<>();
+    //All ballots are initially stored here
     private List<Ballot> ballots= new ArrayList<>();
 
     public BallotBox (String file) {
         try (Scanner sc = new Scanner(new File(file))) {
-            names = sc.nextLine().split(",");
+            String[] names = Arrays.copyOfRange(sc.nextLine().split(","), 1, 3);
+            for (String name : names) {
+                candidates.add(new Candidate(name));
+            }
             while (sc.hasNextLine()) {
                 int[] votes = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
                 ballots.add(new Ballot(
@@ -18,10 +23,31 @@ public class BallotBox {
             }
         } catch (Exception e) {
             System.out.println("File not Found");
+            return;
         }
     }
 
     public List<Ballot> getBallots() {
         return ballots;
+    }
+
+    public void assignVotes () {
+        if (ballots.size() == 0) {
+            System.out.println("No ballots found");
+            return;
+        }
+        for (Ballot b : ballots) {
+            if (b.getChoice() == 1) {
+                candidates.get(0).addVote();
+            } else {
+                candidates.get(1).addVote();
+            }
+        }
+    }
+
+    public String getWinner() {
+        List<Candidate> sortedCandides = candidates;
+        sortedCandides.sort((a, b) -> {return -1 * a.compareTo(b);});
+        return candidates.getFirst().getName();
     }
 }

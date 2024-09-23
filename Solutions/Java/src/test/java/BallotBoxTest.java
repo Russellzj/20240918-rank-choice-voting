e45d8574bcc07790388752daab43f5192dfb2996
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-23 12:26:40 -0400

    Changed winner test to param test

--- Solutions/Java/src/test/java/BallotBoxTest.java
+++ Solutions/Java/src/test/java/BallotBoxTest.java
@@ -2,16 +2,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class BallotBoxTest {
     BallotBox ballotBox;
-    String csvTest1 = "../../test-elections/test-01-basic-majority/cvr.csv";
-
+    final String csvTest1 = "../../test-elections/test-01-basic-majority/cvr.csv";
+    final String csvTest2 = "../../test-elections/test-02-no-initial-majority-candidate/cvr.csv";
 
     @Test
     @DisplayName("No File Test")
@@ -26,14 +26,14 @@ public class BallotBoxTest {
         assert(ballotBox.getBallots().getFirst().getChoice() == 1);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Find correct victor")
-    public void testVictor() {
-        ballotBox = new BallotBox(csvTest1);
+    @CsvSource(value = {csvTest1 + ",Vim", csvTest2 + ",Bob"})
+    public void testVoting(String file, String expectedWinner) {
+        ballotBox = new BallotBox(file);
         ballotBox.assignVotes();
         String winner = ballotBox.getWinner();
-        assert(winner.equals("Vim"));
+        System.out.println(winner);
+        assert(winner.equals(expectedWinner));
     }
-
-
 }

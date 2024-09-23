5bfd85fbe93a1a11f235f477fdc72076899d5bb2
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-23 12:02:21 -0400

    Created variables, gets and compareTo

--- Solutions/Java/src/main/java/Candidate.java
+++ Solutions/Java/src/main/java/Candidate.java
@@ -1,2 +1,24 @@
-public class Candidate {
+//Class to hold voter names and their assigned votes
+public class Candidate implements Comparable<Candidate>{
+    private String name;
+    private int votes;
+
+    public Candidate (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addVote() {
+        votes++;
+    }
+
+    //Compares Candidate's votes to another Candidate's
+    @Override
+    public int compareTo(Candidate otherCandidate) {
+        return Integer.compare(votes, otherCandidate.votes);
+
+    }
 }

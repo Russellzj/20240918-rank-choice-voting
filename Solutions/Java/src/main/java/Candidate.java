af8b1af3f88e8c7e4d4aff23c681d6a3176b6947
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-23 22:55:52 -0400

    Added ID variable and get

--- Solutions/Java/src/main/java/Candidate.java
+++ Solutions/Java/src/main/java/Candidate.java
@@ -2,17 +2,27 @@
 public class Candidate implements Comparable<Candidate>{
     private String name;
     private int votes;
+    private int id;
 
-    public Candidate (String name) {
+    public Candidate (String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getId(){
+        return id;
+    }
+
     public void addVote() {
         votes++;
+    }
+
+    public int getVotes() {
+        return votes;
     }
 
     //Compares Candidate's votes to another Candidate's

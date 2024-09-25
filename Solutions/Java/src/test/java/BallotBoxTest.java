f00d5feb1d82bf0e84c2eb00faffdfab220405b6
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-25 15:16:25 -0400

    Added csvTest5 and a test for testing a single csv file

--- Solutions/Java/src/test/java/BallotBoxTest.java
+++ Solutions/Java/src/test/java/BallotBoxTest.java
@@ -9,6 +9,7 @@ public class BallotBoxTest {
     final String csvTest2 = "../../test-elections/test-02-no-initial-majority-candidate/cvr.csv";
     final String csvTest3 = "../../test-elections/test-03-multiple-rounds-of-elimination/cvr.csv";
     final String csvTest4 = "../../test-elections/test-04-exhausted-ballot/cvr.csv";
+    final String csvTest5 = "../../test-elections/test-05-no-majority--all-candidates-eliminated-except-one/cvr.csv";
 
     @Test
     @DisplayName("No File Test")
@@ -18,7 +19,7 @@ public class BallotBoxTest {
 
     @ParameterizedTest
     @DisplayName("Find correct winner")
-    @CsvSource(value = {csvTest1 + ",Vim", csvTest2 + ",Bob", csvTest3 + ",Bob", csvTest4 + ",Alice"})
+    @CsvSource(value = {csvTest1 + ",Vim", csvTest2 + ",Bob", csvTest3 + ",Bob", csvTest4 + ",Alice", csvTest5 + ",Alice"})
     public void testWinning(String file, String expectedWinner) {
         ballotBox = new BallotBox(file);
         ballotBox.setVotes();
@@ -26,5 +27,15 @@ public class BallotBoxTest {
         System.out.println("Winner: " + winner);
         System.out.println("Rounds: " + ballotBox.round);
         assert (winner.equals(expectedWinner));
+    }
+
+    @Test
+    @DisplayName("Test Single File")
+    public void testSingleFile() {
+        ballotBox = new BallotBox(csvTest4);
+        ballotBox.setVotes();
+        String winner = ballotBox.getWinnerName();
+        System.out.println("Winner: " + winner);
+        System.out.println("Rounds: " + ballotBox.round);
     }
 }

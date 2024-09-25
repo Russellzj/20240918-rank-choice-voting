590396a17d49db56f325d309bf5bceda597a1f85
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-25 19:13:36 -0400

    Added a check to see if the ballot is valid Ex: repeated ranks for different candidates

--- Solutions/Java/src/main/java/BallotBox.java
+++ Solutions/Java/src/main/java/BallotBox.java
@@ -16,24 +16,32 @@ public class BallotBox {
     public BallotBox (String file) {
         try (Scanner sc = new Scanner(new File(file))) {
             String[] namesSplit = sc.nextLine().split(",");
-
             for (int i = 1; i < namesSplit.length; i++) {
                 candidates.put(i, new Candidate(namesSplit[i], i));
             }
             while (sc.hasNextLine()) {
-
                 String[] importedVotes = sc.nextLine().split(",");
                 int[] votes = new int[importedVotes.length];
+                //used to hold ranks already used to make sure the ballot is acceptable
+                Set<String> usedRanks = new HashSet<>();
+                //Changes to false if the ballot is not valid
+                boolean isValid = true;
                 for (int i = 0; i < votes.length; i++) {
                     if (importedVotes[i].isBlank()) {
                         votes[i] = 0;
-                    } else {
+                    } else if (!usedRanks.contains(importedVotes[i])) {
                         votes[i] = Integer.parseInt(importedVotes[i]);
+                        usedRanks.add(importedVotes[i]);
+                    } else {
+                        isValid = false;
+                        break;
                     }
                 }
-
-                //ignores the first element of the ballot since we do not need ballot ID
-                ballots.add(new Ballot(Arrays.copyOfRange(votes, 1, votes.length)));
+                //Only add vote to ballots if the ballot is valid
+                if (isValid) {
+                    //ignores the first element of the ballot since we do not need ballot ID
+                    ballots.add(new Ballot(Arrays.copyOfRange(votes, 1, votes.length)));
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
